f0dee4ac1929e8c949f476ec7d4dc41813b11796
Lisa Wray <lisa@genius.com>
2016-10-07 13:05:26 -0400

    Come back and clean this up, it's working

--- example/src/main/java/com/genius/groupie/example/ExpandableHeaderItem.java
+++ example/src/main/java/com/genius/groupie/example/ExpandableHeaderItem.java
@@ -1,5 +1,6 @@
 package com.genius.groupie.example;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.support.annotation.StringRes;
 import android.view.View;
 
@@ -18,7 +19,8 @@ public class ExpandableHeaderItem extends HeaderItem implements ExpandableItem {
 
     @Override public void bind(final ItemHeaderBinding viewBinding, int position) {
         super.bind(viewBinding, position);
-        bindIcon(viewBinding);
+        viewBinding.icon.setVisibility(View.VISIBLE);
+        viewBinding.icon.setImageResource(expandableGroup.isExpanded() ? R.drawable.collapse : R.drawable.expand);
         viewBinding.icon.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 expandableGroup.onToggleExpanded();
@@ -29,8 +31,9 @@ public class ExpandableHeaderItem extends HeaderItem implements ExpandableItem {
 
     private void bindIcon(ItemHeaderBinding viewBinding) {
         viewBinding.icon.setVisibility(View.VISIBLE);
-        viewBinding.icon.setImageResource(
-                expandableGroup.isExpanded() ? R.drawable.collapse : R.drawable.expand);
+        viewBinding.icon.setImageResource(expandableGroup.isExpanded() ? R.drawable.collapse_animated : R.drawable.expand_animated);
+        AnimatedVectorDrawable drawable = (AnimatedVectorDrawable) viewBinding.icon.getDrawable();
+        drawable.start();
     }
 
     @Override public void setExpandableGroup(ExpandableGroup onToggleListener) {
